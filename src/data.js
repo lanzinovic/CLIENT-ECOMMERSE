ae0ce228216183b57ecb2a9ab554da7864a3dc9e
starboy <vkiplangat553@gmail.com>
2024-02-14 17:28:08 +0300

    commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/474x/7a/f2/5c/7af25c0cf897d57a3f9c5562b8e48f56.jpg",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/474x/90/32/ca/9032ca35f9589dc2b4f05b98f68b6f56.jpg",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/474x/94/ef/08/94ef0839685b83c281eb0ca815e2a221.jpg",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/474x/80/7e/c5/807ec5aa3026d09dab2e8dbc04293a84.jpg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://i.pinimg.com/474x/70/6a/71/706a712faca127daa85f2163a16948a2.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/474x/d0/40/62/d040626a264685d430abfe40acedbff1.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://i.pinimg.com/474x/31/4f/a5/314fa52e0bc7fc0ee993166c3bcb7a8a.jpg",
+  },
+  {
+    id:2,
+    img:"https://i.pinimg.com/474x/c4/49/b1/c449b187c3fdfc03c03d30a14f5b4409.jpg",
+  },
+  {
+    id:3,
+    img:"https://i.pinimg.com/474x/f5/05/5f/f5055fcd7181d1f062a086b35e472df9.jpg",
+  },
+  {
+    id:4,
+    img:"https://i.pinimg.com/474x/0f/5b/31/0f5b310256dbaf39a7011e4f8b130735.jpg",
+  },
+  {
+    id:5,
+    img:"https://i.pinimg.com/736x/58/82/cb/5882cbad328e457618b6450e0872f96d.jpg",
+  },
+  {
+    id:6,
+    img:"https://i.pinimg.com/474x/99/cb/03/99cb03a50df4d041ea23ef8e0090daf3.jpg",
+  },
+  {
+    id:7,
+    img:"https://i.pinimg.com/474x/d1/b7/78/d1b778ac2fe2783d985991a2bac6e3e6.jpg",
+  },
+  {
+    id:8,
+    img:"https://i.pinimg.com/474x/88/59/5a/88595a46c5a57cf66c40c01a54b66d07.jpg",
+  },
+]
